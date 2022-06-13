de6da75abf6fabf2c22146626f875a91961e5bdd
sebastiansmoter <107245216+sebastiansmoter@users.noreply.github.com>
2022-06-13 07:50:07 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+version = "3.5.0"
+project = "mythic-tribute-352813"
+region = "us-central1"
+zone = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+name = "terraform-instance2"
+machine_type = "f1-micro"
+zone = "us-central1-c"
+boot_disk {
+initialize_params {
+image = "centos-cloud/centos-7"
+}
+}
+
+network_interface {
+network = google_compute_network.vpc_network.name
+access_config {
+}
+}
+}
